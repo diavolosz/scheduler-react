eca52a7c30f49585bff66a9f00ed8ee89e9cb86c
le duong hien thong <michaeldiavolos@gmail.com>
2022-07-14 01:19:15 -0400

    completed schedule list displaying booked slots

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header.js";
 import Show from "components/Appointment/Show";
@@ -8,7 +8,7 @@ function Appointment(props) {
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? <Show student={props.student} interviewer={props.interviewer}/> :  <Empty />}
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer.name}/> :  <Empty />}
     </article>
   )
 }
